e034c9974cd1c390d6a5cde79c3165d9eacc7c2f
sc-searchengineunity <sharon@searchengeineunity.com>
2024-04-16 12:29:31 -0400

    change react react dom experimental version

--- src/pages/using-partial-hydration.js
+++ src/pages/using-partial-hydration.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import { Demo } from "../components/demo"
+import { GatsbyImage } from 'gatsby-plugin-image';
 
 function usingPartialHydration() {
   return (
